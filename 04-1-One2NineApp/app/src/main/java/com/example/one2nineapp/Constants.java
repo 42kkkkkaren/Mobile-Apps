8397dd685f941dc6ce8d95bd6fd918e6fe1eabdd
Karen Ketlyn Ferreira Barcelos <72056327+42kkkkkaren@users.noreply.github.com>
2024-04-24 22:47:04 -0300

    Delete 04-1-One2NineApp directory

--- 04-1-One2NineApp/app/src/main/java/com/example/one2nineapp/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.example.one2nineapp;
-
-public class Constants {
-    public static String PREFS_FILE_NAME = "One2NinePrefs";
-    public static String PREFS_TIME = "time";
-    public static String PREFS_PLAYER = "player";
-    public static String PREFS_WHEN = "when";
-
-}
